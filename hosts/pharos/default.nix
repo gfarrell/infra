2280d21fb7bed5fa5ae69a912f1fc63e804bc728
Gideon Farrell <gideon@gtf.io>
2024-07-07 18:36:50 +0100

    add grafana and loki/promtail monitoring

--- hosts/pharos/default.nix
+++ hosts/pharos/default.nix
@@ -33,6 +33,10 @@ in {
       encode gzip
       reverse_proxy localhost:${toString config.services.prometheus.port}
     '';
+    virtualHosts.${config.services.grafana.settings.server.domain}.extraConfig = ''
+      encode gzip
+      reverse_proxy localhost:${toString config.services.grafana.settings.server.http_port}
+    '';
   };
 
   # configure the gtf-io website module
@@ -72,6 +76,129 @@ in {
         ];
       }
     ];
+  };
+  # Use grafana for graphing metrics and logs
+  services.grafana = {
+    enable = true;
+    settings.server = {
+      http_addr = "127.0.0.1";
+      http_port = 9080;
+      domain = "monitor.gtf.io";
+    };
+
+    provision = {
+      enable = true;
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          access = "proxy";
+          url = "http://127.0.0.1:${toString config.services.prometheus.port}";
+        }
+        {
+          name = "Loki";
+          type = "loki";
+          access = "proxy";
+          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
+        }
+      ];
+    };
+  };
+
+  # Ingest logs with loki
+  services.loki = {
+    enable = true;
+    configuration = {
+      server.http_listen_port = 9085;
+      auth_enabled = false;
+      ingester = {
+        lifecycler = {
+          address = "127.0.0.1";
+          ring = {
+            kvstore.store = "inmemory";
+            replication_factor = 1;
+          };
+        };
+        chunk_idle_period = "1h";
+        max_chunk_age = "1h";
+        chunk_target_size = 999999;
+        chunk_retain_period = "30s";
+      };
+      schema_config = {
+        configs = [
+          {
+            from = "2024-07-01";
+            schema = "v13";
+            store = "tsdb";
+            object_store = "gcs";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }
+        ];
+      };
+      storage_config = {
+        tsdb_shipper = {
+          active_index_directory = "/var/lib/loki/tsdb-index";
+          cache_location = "/var/lib/loki/tsdb-cache";
+        };
+        filesystem.directory = "/var/lib/loki/chunks";
+      };
+      query_scheduler = {
+        max_outstanding_requests_per_tenant = 32768;
+      };
+      querier = {
+        max_concurrent = 16;
+      };
+      limits_config = {
+        reject_old_samples = true;
+        reject_old_samples_max_age = "168h";
+      };
+      table_manager = {
+        retention_deletes_enabled = false;
+        retention_period = "0s";
+      };
+      compactor = {
+        working_directory = "/var/lib/loki";
+        compactor_ring.kvstore.store = "inmemory";
+      };
+    };
+  };
+
+  # promtail collects system logs
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        http_listen_port = 9086;
+        grpc_listen_port = 0;
+      };
+      positions.filename = "/tmp/positions.yaml";
+      clients = [
+        {
+          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+        }
+      ];
+      scrape_configs = [
+        {
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = config.networking.hostName;
+            };
+          };
+          relabel_configs = [
+            {
+              source_labels = ["__journal__systemd_unit"];
+              target_label = "unit";
+            }
+          ];
+        }
+      ];
+    };
   };
 
   networking.hostName = "pharos";
